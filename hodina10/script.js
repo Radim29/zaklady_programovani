26f67ecf5081f464aefb27cc9b7221fb29e6af92
Radim29 <115022195+Radim29@users.noreply.github.com>
2022-11-28 18:30:45 +0100

    Add files via upload

---
+++ hodina10/script.js
@@ -0,0 +1,29 @@
+function myEvent()
+{
+console.log("Hello world");
+let mojePromena = 5;
+let toMensi = 3;
+console.log(mojePromena + " > "+ toMensi);
+if(mojePromena > 3)
+{
+    console.log ("je vetsi");
+}
+  else  
+{
+    console.log("je mensi");
+
+}
+
+mojePromena = 2;
+
+if(mojePromena > 3)
+{
+    console.log ("je vetsi");
+}
+  else  
+{
+    console.log("je mensi");
+
+}
+
+}
